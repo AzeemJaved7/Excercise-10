09a7c185bd73024d042cc631bbecbfab46c2bd72
AzeemJaved7 <163923406+AzeemJaved7@users.noreply.github.com>
2024-03-21 01:10:54 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+"use strict";
+//Adding Comments
+let ProgrammeOne = ("I m learning Basic lenguage");
+let ProgrammeTwo = ("& vasual Studio and many more");
+let Name = (("Azeem, "));
+// let Class: string=("Monday 9-12 am")
+let RollNo = (429124);
+let messagethree = `My Rool No. is ${RollNo}`;
+let messageOne = `My name is ${Name}`;
+let messagefour = `Class: Monday 9 to 12 Am`;
+let date = ("19 March 2024,");
+let MessageTwo = `Today's date is ${date}`;
+console.log(messageOne, MessageTwo, ProgrammeOne);
+console.log(ProgrammeTwo);
+console.log(messagethree, messagefour);
---
+++ app.ts
@@ -0,0 +1,13 @@
+//Adding Comments
+let ProgrammeOne: String=( "I m learning Basic lenguage" );
+let ProgrammeTwo: String=("& vasual Studio and many more");
+let Name: string=( ("Azeem, ") );
+let RollNo:number= (429124)
+let messagethree: string= `My Rool No. is ${RollNo}` 
+let messageOne: string= `My name is ${Name}`;
+let messagefour: string= `Class: Monday 9 to 12 Am`
+let date: string=("19 March 2024,");
+let MessageTwo: string= `Today's date is ${date}`; 
+console.log ( messageOne,      MessageTwo,    ProgrammeOne)     
+console.log (    ProgrammeTwo  )
+console.log ( messagethree , messagefour )
